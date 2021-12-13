ff20e891b16218bbddc324e9bc049957af1d2045
effervescent000 <kislio01@gmail.com>
2021-12-13 11:27:54 -0500

    generating queue clears out old items

--- ffxivledger/static/dashboard.js
+++ ffxivledger/static/dashboard.js
@@ -2,11 +2,17 @@ function getCraftQueue(num, currentUser) {
     if (typeof num === "string") {
         num = parseInt(num)
     }
+    // first clear out any currently listed crafting queue items
+    const parent = document.getElementById("crafting-output-wrapper")
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+    
+    // next retrieve the crafting queue from the backend
     fetch(`/craft/get_queue/${currentUser}-${num}`)
     .then(data => data.json())
     .then(data => {
-        console.log(data)
-        const parent = document.getElementById("crafting-output-wrapper")
+        // console.log(data)
         const craftList = data[0]
         craftList.forEach(craft => {
             const craftDiv = document.createElement('div')
